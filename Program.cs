aa4f0c295d3769153d5e0c96ece95d6f66ac5425
cgrant61289 <cgrant61289@gmail.com>
2026-08-04 14:42:19 -0400

    updated scope

--- Program.cs
+++ Program.cs
@@ -134,4 +134,10 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .DisableAntiforgery();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
