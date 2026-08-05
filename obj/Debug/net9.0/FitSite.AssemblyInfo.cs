1b4568dc3ddcd654cd3036fa1dc66623ec6ede50
cgrant61289 <cgrant61289@gmail.com>
2026-08-05 10:18:27 -0400

    Made add exercise panel collapsible

--- obj/Debug/net9.0/FitSite.AssemblyInfo.cs
+++ obj/Debug/net9.0/FitSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f27eb2c1aa9a4bb02b266a6b67b2ad41356d4dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+489673435c02db8e423bba31c8c01ea7d4b436a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
